34070bae530518c3f44d141ab3ba9b53e8f24b82
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-11-16 17:23:07 +0100

    ButtonLed

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+IDENTITY=../../.ssh/raspberry-pi
+URL=pi@raspberrypi.local
+JAR=raspberry-pi-1.0-SNAPSHOT.jar
+mvn package 
+scp -i "$IDENTITY" "target/$JAR" "$URL:/home/pi" 
+ssh -i "$IDENTITY" "$URL" java -cp "$JAR" "com.github.maximtereshchenko.$1"
---
+++ src/main/java/com/github/maximtereshchenko/ButtonLed.java
@@ -0,0 +1,22 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.devices.Button;
+import com.diozero.devices.LED;
+
+import java.time.Instant;
+
+final class ButtonLed {
+
+    public static void main(String[] args) {
+        var deadline = Instant.now().plusSeconds(30);
+        try (var led = new LED(17); var button = new Button(18)) {
+            while (Instant.now().isBefore(deadline)) {
+                if (button.isActive()) {
+                    led.off();
+                } else {
+                    led.on();
+                }
+            }
+        }
+    }
+}
