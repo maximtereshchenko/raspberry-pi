f612ddcbeb33c03acab4d465d718b40ea93cb831
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-11-20 18:57:20 +0100

    NightLamp

---
+++ src/main/java/com/github/maximtereshchenko/NightLamp.java
@@ -0,0 +1,22 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.devices.PwmLed;
+
+import java.time.Instant;
+
+final class NightLamp {
+
+    public static void main(String[] args) {
+        var adc = new ADS7830Device();
+        try (var led = new PwmLed(17)) {
+            var deadline = Instant.now().plusSeconds(30);
+            while (Instant.now().isBefore(deadline)) {
+                led.setValue(dutyCycle(adc.analogRead(0)));
+            }
+        }
+    }
+
+    private static float dutyCycle(int value) {
+        return value / 255.0f;
+    }
+}
