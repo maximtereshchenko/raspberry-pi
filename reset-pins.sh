6653ff85346c492fa9a0357818002726bd06377c
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-11-23 23:17:56 +0100

    Relay

--- reset-pins.sh
+++ reset-pins.sh
@@ -8,5 +8,6 @@ ssh -i "$IDENTITY" "$URL" <<EOF
 for i in {0..31}
 do
   gpio mode "\$i" in
+  gpio write "\$i" 0
 done
 EOF
---
+++ src/main/java/com/github/maximtereshchenko/Relay.java
@@ -0,0 +1,20 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.api.GpioPullUpDown;
+import com.diozero.devices.Button;
+import com.diozero.devices.LED;
+
+import java.util.concurrent.TimeUnit;
+
+final class Relay {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (
+            var button = new Button(18, GpioPullUpDown.PULL_UP);
+            var led = new LED(17)
+        ) {
+            button.whenPressed(time -> led.toggle());
+            TimeUnit.SECONDS.sleep(30);
+        }
+    }
+}
