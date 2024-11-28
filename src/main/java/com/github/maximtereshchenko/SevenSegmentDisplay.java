b684868fd9fc3ee054fe23dc8de200e0f7604c8c
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-11-28 22:37:24 +0100

    SevenSegmentDisplay

---
+++ src/main/java/com/github/maximtereshchenko/SevenSegmentDisplay.java
@@ -0,0 +1,51 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.api.DigitalOutputDevice;
+
+import java.util.concurrent.TimeUnit;
+
+final class SevenSegmentDisplay {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (
+            var latch = new DigitalOutputDevice(27);
+            var clock = new DigitalOutputDevice(22);
+            var data = new DigitalOutputDevice(17)
+        ) {
+            display(
+                latch,
+                clock,
+                data,
+                0b00000011, //0
+                0b10011110, //1.
+                0b00100101, //2
+                0b00001100, //3.
+                0b10011001, //4
+                0b01001000, //5.
+                0b11000001, //6
+                0b00011110, //7.
+                0b00000001, //8
+                0b00011000 //9.
+            );
+        }
+    }
+
+    private static void display(DigitalOutputDevice latch, DigitalOutputDevice clock, DigitalOutputDevice data, int... bitMasks) throws InterruptedException {
+        for (var bitMask : bitMasks) {
+            output(latch, clock, data, bitMask);
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    private static void output(DigitalOutputDevice latch, DigitalOutputDevice clock, DigitalOutputDevice data, int value) {
+        int remaining = value;
+        latch.off();
+        for (var i = 0; i < 8; i++) {
+            clock.off();
+            data.setOn((remaining & 1) == 1);
+            clock.on();
+            remaining = remaining >> 1;
+        }
+        latch.on();
+    }
+}
