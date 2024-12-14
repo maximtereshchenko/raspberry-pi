cfb53acec56516539183b5e0521b52873e9b1405
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-12-14 21:20:47 +0100

    UltrasonicRanging

---
+++ src/main/java/com/github/maximtereshchenko/UltrasonicRanging.java
@@ -0,0 +1,19 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.devices.HCSR04;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+final class UltrasonicRanging {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (var sensor = new HCSR04(23, 24)) {
+            var deadline = Instant.now().plusSeconds(15);
+            while (Instant.now().isBefore(deadline)) {
+                System.out.printf("%2f cm%n", sensor.getDistanceCm());
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        }
+    }
+}
