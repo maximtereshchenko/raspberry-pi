dab4bce96a02895c1c1637aaf2fefe6c52a181b7
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-12-21 22:12:50 +0100

    TouchSensor

--- src/main/java/com/github/maximtereshchenko/TouchSensor.java
+++ src/main/java/com/github/maximtereshchenko/TouchSensor.java
@@ -2,26 +2,28 @@ package com.github.maximtereshchenko;
 
 import com.diozero.api.GpioPullUpDown;
 import com.diozero.api.SmoothedInputDevice;
-import com.diozero.devices.LED;
+import com.diozero.devices.PwmLed;
 import com.diozero.util.DiozeroScheduler;
 
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
-final class VoiceLamp {
+final class TouchSensor {
 
     public static void main(String[] args) throws InterruptedException {
         try (
-            var microphone = new SmoothedInputDevice(18, GpioPullUpDown.PULL_DOWN, 5, 10, 20);
-            var led = new LED(17)
+            var touchSensor = new SmoothedInputDevice(18, GpioPullUpDown.PULL_DOWN, 5, 10, 20);
+            var led = new PwmLed(17)
         ) {
+            var counter = 1;
             var deadline = Instant.now().plusSeconds(30);
             while (Instant.now().isBefore(deadline)) {
-                if (microphone.isActive()) {
-                    led.on();
-                    TimeUnit.MILLISECONDS.sleep(500);
-                } else {
-                    led.off();
+                if (touchSensor.isActive()) {
+                    if (counter > 3) {
+                        counter = 0;
+                    }
+                    led.setValue(1f / 3 * counter++);
+                    TimeUnit.SECONDS.sleep(1);
                 }
             }
         } finally {
