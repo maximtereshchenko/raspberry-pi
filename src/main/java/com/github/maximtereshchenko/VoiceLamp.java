cb86019df570343967d205f8339f6637999159aa
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-12-21 21:36:43 +0100

    VoiceLamp

---
+++ src/main/java/com/github/maximtereshchenko/VoiceLamp.java
@@ -0,0 +1,30 @@
+package com.github.maximtereshchenko;
+
+import com.diozero.api.GpioPullUpDown;
+import com.diozero.api.SmoothedInputDevice;
+import com.diozero.devices.LED;
+import com.diozero.util.DiozeroScheduler;
+
+import java.time.Instant;
+import java.util.concurrent.TimeUnit;
+
+final class VoiceLamp {
+
+    public static void main(String[] args) throws InterruptedException {
+        try (
+            var microphone = new SmoothedInputDevice(18, GpioPullUpDown.PULL_DOWN, 5, 10, 20);
+            var led = new LED(17)
+        ) {
+            var deadline = Instant.now().plusSeconds(30);
+            while (Instant.now().isBefore(deadline)) {
+                if (microphone.isActive()) {
+                    led.on();
+                    TimeUnit.MILLISECONDS.sleep(500);
+                } else {
+                    led.off();
+                }
+            }
+            DiozeroScheduler.shutdownAll();
+        }
+    }
+}
