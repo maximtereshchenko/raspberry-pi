f11bb33ef88d2fe4859ca5545bb665e07821832c
maximtereshchenko <tereshchenko.xini@gmail.com>
2024-12-25 22:37:16 +0100

    MerryChristmas

--- src/main/java/com/github/maximtereshchenko/FlowingWaterLight.java
+++ src/main/java/com/github/maximtereshchenko/FlowingWaterLight.java
@@ -1,11 +1,5 @@
 package com.github.maximtereshchenko;
 
-import com.diozero.devices.LED;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
-
 final class FlowingWaterLight {
 
     public static void main(String[] args) throws InterruptedException {
@@ -13,37 +7,6 @@ final class FlowingWaterLight {
             for (var i = 0; i < 10; i++) {
                 leds.flow();
             }
-        }
-    }
-
-    private static final class Leds implements AutoCloseable {
-
-        private List<LED> all;
-
-        private Leds(List<LED> all) {
-            this.all = all;
-        }
-
-        static Leds from(Integer... pins) {
-            return new Leds(
-                Stream.of(pins)
-                    .map(LED::new)
-                    .toList()
-            );
-        }
-
-        @Override
-        public void close() {
-            all.forEach(LED::close);
-        }
-
-        void flow() throws InterruptedException {
-            for (var led : all) {
-                led.on();
-                TimeUnit.MILLISECONDS.sleep(100);
-                led.off();
-            }
-            all = all.reversed();
         }
     }
 }
