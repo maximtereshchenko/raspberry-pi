9e41d519c48eaf172138f3542baa4f775d894759
maximtereshchenko <tereshchenko.xini@gmail.com>
2025-01-06 16:50:52 +0100

    Rfid

--- run.sh
+++ run.sh
@@ -2,9 +2,8 @@
 
 set -Eeuo pipefail
 
-IDENTITY=~/.ssh/raspberry-pi
 URL=pi@raspberrypi.local
 JAR=raspberry-pi-1.0-SNAPSHOT.jar
 mvn clean package 
-scp -i "$IDENTITY" "target/$JAR" "$URL:/home/pi" 
-ssh -i "$IDENTITY" "$URL" sudo java -cp "$JAR" "com.github.maximtereshchenko.$1"
+scp "target/$JAR" "$URL:/home/pi" 
+ssh "$URL" sudo java -cp "$JAR" "com.github.maximtereshchenko.$1"
